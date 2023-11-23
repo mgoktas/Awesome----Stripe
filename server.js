e0a913700dce2895a61a3d15e06f90ffdef8c76d
Muhammet <mgoktashk@gmail.com>
2023-11-23 15:45:57 +0300

    kuhsada

--- server.js
+++ server.js
@@ -29,35 +29,35 @@ app.use("/.well-known/apple-app-site-association", function (req, res) {
     res.sendFile(path.join(__dirname, "", ".well-known/apple-app-site-association"))
 } )
 
-// const stripe = require('stripe')('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');
+const stripe = require('stripe')('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');
 // // const stripe = require('stripe')('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');
 const PORT = process.env.PORT || 3000
 
-// app.post('/payment-sheet', async (req, res) => {
-//   // Use an existing Customer ID if this is a returning customer.
-//   const customer = await stripe.customers.create();
-//   const ephemeralKey = await stripe.ephemeralKeys.create(
-//     {customer: customer.id},
-//     {apiVersion: '2022-11-15'}
-//   );
+app.post('/payment-sheet', async (req, res) => {
+  // Use an existing Customer ID if this is a returning customer.
+  const customer = await stripe.customers.create();
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    {customer: customer.id},
+    {apiVersion: '2022-11-15'}
+  );
   
-//   const paymentIntent = await stripe.paymentIntents.create({
-//     amount: 20,
-//     currency: 'eur',
-//     customer: customer.id,
-//     // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
-//     automatic_payment_methods: {
-//       enabled: true,
-//     },
-//   });
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 20,
+    currency: 'eur',
+    customer: customer.id,
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
 
-//   res.json({
-//     paymentIntent: paymentIntent.client_secret,
-//     ephemeralKey: ephemeralKey.secret,
-//     customer: customer.id,
-//     // publishableKey: 'pk_live_51MzWeiEYzAPwGPE13wscXu5RF5KI12zxxPgLcDS4fMW6T1DOlAjNqREMP2g5SIfDeZVSGtvQuAj8bpQMIGmYrt5U00ZHCar5IV'
-//   });
-// });
+  res.json({
+    paymentIntent: paymentIntent.client_secret,
+    ephemeralKey: ephemeralKey.secret,
+    customer: customer.id,
+    // publishableKey: 'pk_live_51MzWeiEYzAPwGPE13wscXu5RF5KI12zxxPgLcDS4fMW6T1DOlAjNqREMP2g5SIfDeZVSGtvQuAj8bpQMIGmYrt5U00ZHCar5IV'
+  });
+});
 
 app.use('/a', async(req, res) => {
     res.render('index.html', { layout: false });
