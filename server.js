9a98355fa9c13d724576985b64bdb13ce443dea7
Muhammet <mgoktashk@gmail.com>
2023-12-03 23:33:30 +0300

    kl

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.use("/.well-known/apple-app-site-association", async function (req, res) {
     res.sendFile(path.join(__dirname, "", ".well-known/apple-app-site-association"))
 } )
 
-const stripe = require('stripe')('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');
-// // const stripe = require('stripe')('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');
+// const strkipe = require('stripe')('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');
+const stripe = require('stripe')('sk_test_51MzWeiEYzAPwGPE1vKrubfWOhfFxWYxotsGYVSdS8QcXWHwNk1IcLOzIqsZPhSRGymalUo8TrAAIQrnl0eLiCmHh00WRnMD5Wg');
 const PORT = process.env.PORT || 3000
 
 app.post('/payment-sheet', async (req, res) => {
@@ -45,7 +45,7 @@ app.post('/payment-sheet', async (req, res) => {
   );
   
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: 490,
+    amount: 499,
     currency: 'usd',
     customer: customer.id,
     // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
@@ -56,10 +56,146 @@ app.post('/payment-sheet', async (req, res) => {
 
   res.json({
     paymentIntent: paymentIntent.client_secret,
+    amount: paymentIntent.amount,
     ephemeralKey: ephemeralKey.secret,
     customer: customer.id,
     // publishableKey: 'pk_live_51MzWeiEYzAPwGPE13wscXu5RF5KI12zxxPgLcDS4fMW6T1DOlAjNqREMP2g5SIfDeZVSGtvQuAj8bpQMIGmYrt5U00ZHCar5IV'
   });
+});
+
+app.post('/create-checkout-session', async (req, res) => {
+
+  const {priceId} = req.query
+  const customer = await stripe.customers.create();
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    {customer: customer.id},
+    {apiVersion: '2022-11-15'}
+  );
+
+  const subscription = await stripe.subscriptions.create({
+    customer: customer.id,
+    items: [
+      {
+        price: 'price_1OGDO2EYzAPwGPE1sx7e0bOP',
+      },
+    ],
+    payment_behavior: 'default_incomplete',
+    expand: ['latest_invoice.payment_intent'],
+    trial_period_days: 7
+  });
+
+
+  
+  
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 499,
+    currency: 'usd',
+    customer: customer.id,
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+    metadata: {
+      subscriptionId: subscription.id,
+      reason: 'subscription',
+    },
+  });
+  
+  // const session = await stripe.checkout.sessions.create({
+  //   mode: 'subscription',
+  //   line_items: [
+  //     {
+  //       price: priceId,
+  //       // For metered billing, do not pass quantity
+  //       quantity: 1,
+  //     },
+  //   ],
+  //   // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
+  //   // the actual Session ID is returned in the query parameter when your customer
+  //   // is redirected to the success page.
+  //   success_url: "http://will-doro-ff47e2266450.herokuapp.com/success?email=email",
+  //   success_url: "http://yoursite.com/order/success?session_id={CHECKOUT_SESSION_ID}",
+  //   cancel_url: 'https://example.com/canceled.html',
+  // });
+  
+    res.json({
+      paymentIntent: paymentIntent.client_secret,
+      amount: paymentIntent.amount,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer.id,
+      // publishableKey: 'pk_live_51MzWeiEYzAPwGPE13wscXu5RF5KI12zxxPgLcDS4fMW6T1DOlAjNqREMP2g5SIfDeZVSGtvQuAj8bpQMIGmYrt5U00ZHCar5IV'
+    });
+
+  // res.redirect(303, 'http://will-doro-ff47e2266450.herokuapp.com/success?email=email')
+});
+
+console.log('499')
+
+app.post('/create-checkout-session2', async (req, res) => {
+  console.log('499')
+
+  const {priceId} = req.query
+  const customer = await stripe.customers.create();
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    {customer: customer.id},
+    {apiVersion: '2022-11-15'}
+  );
+
+  const subscription = await stripe.subscriptions.create({
+    customer: customer.id,
+    items: [
+      {
+        price: priceId,
+      },
+    ],
+    payment_behavior: 'default_incomplete',
+    expand: ['latest_invoice.payment_intent'],
+    trial_period_days: 7
+  });
+
+
+  
+  
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 499,
+    currency: 'usd',
+    customer: customer.id,
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+    metadata: {
+      subscriptionId: subscription.id,
+      reason: 'subscription',
+    },
+  });
+  
+  // const session = await stripe.checkout.sessions.create({
+  //   mode: 'subscription',
+  //   line_items: [
+  //     {
+  //       price: priceId,
+  //       // For metered billing, do not pass quantity
+  //       quantity: 1,
+  //     },
+  //   ],
+  //   // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
+  //   // the actual Session ID is returned in the query parameter when your customer
+  //   // is redirected to the success page.
+  //   success_url: "http://will-doro-ff47e2266450.herokuapp.com/success?email=email",
+  //   success_url: "http://yoursite.com/order/success?session_id={CHECKOUT_SESSION_ID}",
+  //   cancel_url: 'https://example.com/canceled.html',
+  // });
+  
+    res.json({
+      paymentIntent: paymentIntent.client_secret,
+      amount: paymentIntent.amount,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer.id,
+      // publishableKey: 'pk_live_51MzWeiEYzAPwGPE13wscXu5RF5KI12zxxPgLcDS4fMW6T1DOlAjNqREMP2g5SIfDeZVSGtvQuAj8bpQMIGmYrt5U00ZHCar5IV'
+    });
+
+  // res.redirect(303, 'http://will-doro-ff47e2266450.herokuapp.com/success?email=email')
 });
 
 app.use('/a', async(req, res) => {
